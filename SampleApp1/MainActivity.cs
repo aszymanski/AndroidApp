95048b47de0e877a027fcf9b2d994d35fcca983b
Adrian Szymański <adrian.szymanski@avanade.com>
2019-06-03 08:52:45 +0200

    new layout

--- SampleApp1/MainActivity.cs
+++ SampleApp1/MainActivity.cs
@@ -3,17 +3,31 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Webkit;
 
 namespace SampleApp1
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            Button button = FindViewById<Button>(Resource.Id.button1);
+
+
+            button.Click += delegate
+            {
+                button.SetBackgroundColor(Android.Graphics.Color.Blue);
+                SetContentView(Resource.Layout.test);
+            };
+
+
         }
     }
 }
